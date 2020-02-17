23d25556dd554a6d31f9f36a8106216242bad234
Juan Antonio <juanantonio_genial@hotmail.com>
2020-02-17 16:14:25 +0100

    version1

---
+++ gestalum.php
@@ -0,0 +1,88 @@
+<?php
+class gestalum//CLASE GETSALUM
+
+{
+    public function __construct() //Constructor
+
+    {
+
+        $config = parse_ini_file('./assets/config.ini');
+        try {
+            $pdo = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+            $dsn = 'mysql:host=' . $config['server'] . ';dbname=' . $config['base'];
+            $this->conexion = new PDO($dsn, $config['usu'], $config['pas'], $pdo);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            throw $e;
+
+        }
+    }
+
+    /**
+     * Metodo para ejecutar una consulta
+     * @param sql se pasa como parametro la sentencia sql para usar mas adelante
+     * @return datos devuelve los datos
+     *
+     */
+    private function ejecutaConsulta($sql)
+    {
+        try
+        {
+            $resultado = null;
+            if (isset($this->conexion)) {
+                $resultado = $this->conexion->query($sql);
+                return $resultado;
+            }
+
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    /**
+     * Este metodo lo usaremos para comprobar el usuario en el login
+     * @param usuario se le pasa como parametro el nombre de usuario
+     * @param clave se le pasa la clave que comprobará si el usuario es el correcto para dar login
+     *
+     */
+    public function getUser($usuario, $clave)
+    {
+
+        $sql = "SELECT * FROM usuarios";
+        $resultado = $this->ejecutaConsulta($sql);
+        $datos = $resultado->fetch(PDO::FETCH_ASSOC);
+        if ($usuario == $datos['usuario'] && $clave == $datos['clave']) {
+
+            echo '<script type="text/javascript">
+        alert("Bienvenido señor admin");
+        window.location.href="index.php";
+        </script>';
+        } else {
+            echo '<script type="text/javascript">
+        alert("Usted no es el admin de esta web");
+        window.location.href="index.php";
+        </script>';
+        }
+
+    }
+
+    /**
+     * Funcion para listar un profesor en concreto dependiendo del @param cod
+     * @param cod parámetro que pasa el codigo del profesor del cual realizar la búsqueda
+     * @return profesor devuelve el profesor
+     */
+    public function getProfesor($cod){
+        
+        $sql = "SELECT * FROM profesores where cod_prof = $cod";
+        $resultado = $this->ejecutaConsulta($sql);
+        $profesor = $resultado->fetch(PDO::FETCH_ASSOC);
+        if ($cod = $profesor['cod_prof']) {
+            return $profesor;
+        } else {
+            echo '<p><center>No se han encontrado resultados</center></p>';
+        }
+
+
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+require './gestalum.php';
+session_unset();
+$bd = new gestalum;
+if (isset($_POST['usuario']) && isset($_POST['clave'])) {
+    $_SESSION['usuario'] = $_POST['usuario'];
+    $_SESSION['clave'] = $_POST['clave'];
+
+    $bd->getUser($_SESSION['usuario'], $_SESSION['clave']);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea Online 4</title>
+    <!-- Boostrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+    <h1><center>Tarea Online</center></h1>
+    <h3><center>Login</center></h3>
+    <form action="./index.php" method="POST">
+
+    <div class="form-group">
+    <label for="usuario">Usuario</label>
+    <input type="text" class="form-control" name="usuario" id="usuario" placeholder="Introduce usuario" required>
+
+    <label for="contraseña">Contraseña</label>
+    <input type="password" class="form-control" name="clave" id="clave" placeholder="Introduce la contraseña" required>
+
+    </div>
+    <button type="submit" >Login</button>
+
+
+    </form>
+
+
+    <form>
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div>
+</form>
+
+
+</body>
+</html>
