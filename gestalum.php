332126dec14774564cd939fe222d98cbed5288d1
Juan Antonio <juanantonio_genial@hotmail.com>
2020-02-21 02:23:43 +0100

    f

--- gestalum.php
+++ gestalum.php
@@ -121,8 +121,6 @@ class gestalum//CLASE GETSALUM
         }
     }
 
-
-
     /**
      * Metodo para obtener una lista de TODOS los alumnos
      * @return alumnos devuelve cada uno de los alumnos
@@ -134,11 +132,164 @@ class gestalum//CLASE GETSALUM
         $resultado = $this->ejecutaConsulta($sql);
         $alumnos = $resultado->fetchAll(PDO::FETCH_ASSOC);
         if (isset($alumnos)) {
-            return $profesor;
+            return $alumnos;
         } else {
             echo '<p><center>No se han encontrado resultados</center></p>';
         }
 
     }
+
+    /**
+     * Funcion para obtener un módulo en concreto
+     * @param cod codigo del modulo a buscar.
+     * @return modulo devuelve el modulo encontrado
+     */
+    public function getModulo($cod)
+    {
+
+        $sql = "SELECT * FROM modulos where cod_mod = $cod";
+        $resultado = $this->ejecutaConsulta($sql);
+        $modulo = $resultado->fetch(PDO::FETCH_ASSOC);
+        if ($cod = $modulo['cod_mod']) {
+            return $modulo;
+        } else {
+            echo '<p><center>No se han encontrado resultados</center></p>';
+        }
+    }
+
+    /**
+     * Metodo para obtener una lista de TODOS los modulos
+     * @return modulos devuelve los modulos en forma de lista
+     */
+    public function getModuloAll()
+    {
+
+        $sql = "SELECT * FROM modulos ;";
+        $resultado = $this->ejecutaConsulta($sql);
+        $modulos = $resultado->fetchAll(PDO::FETCH_ASSOC);
+        if (isset($modulos)) {
+            return $modulos;
+        } else {
+            echo '<p><center>No se han encontrado resultados</center></p>';
+        }
+
+    }
+
+    /**
+     * Funcion para obtener una lista las asistencias y faltas del modulo
+     * @param codal parametro para pasar el numero de expdiente del alumno
+     * @param codmod parametro para pasar el numero del codigo del modulo
+     * @return asistencias devuelve las asistencias en forma de lista
+     */
+    public function getAsistenciasMod($codal, $codmod)
+    {
+
+        $sql = "SELECT * FROM asistencias WHERE nexped = $codal and cod_mod = $codmod;";
+        $resultado = $this->ejecutaConsulta($sql);
+        $asistencias = $resultado->fetch(PDO::FETCH_ASSOC);
+        if (isset($asistencias)) {
+            return $asistencias;
+        } else {
+            echo '<p><center>No se han encontrado resultados</center></p>';
+        }
+
+    }
+
+    /**
+     * Funcion para eliminar un profesor
+     * @param cod codigo que se pasa por parametro para identificar el profesor a eliminar
+     * @return profesor devuelve la sentencia de eliminacion del profesor
+     */
+
+    public function delProfesor($cod)
+    {
+        $sql = "DELETE FROM profesores WHERE cod_prof = $cod;";
+        $resultado = $this->ejecutaConsulta($sql);
+        $profesor = $resultado->execute();
+        if (isset($profesor)) {
+            echo '<p><center>Profesor borrado correctamente</center></p>';
+            return $profesor;
+        } else {
+            echo '<p><center>No existe ningun profesor con dicho codigo</center></p>';
+        }
+    }
+
+    /**
+     * Funcion para eliminar un alumno
+     * @param cod codigo que se pasa por parametro para identificar el alumno eliminar
+     * @return alumno devuelve la sentencia de eliminacion del alumno
+     */
+
+    public function delAlumno($cod)
+    {
+        $sql = "DELETE FROM alumnos WHERE nexped = $cod;";
+        $resultado = $this->ejecutaConsulta($sql);
+        $alumno = $resultado->execute();
+        if (isset($alumno)) {
+            echo '<p><center>Alumno borrado correctamente</center></p>';
+            return $alumno;
+        } else {
+            echo '<p><center>No existe ningun alumno con dicho codigo</center></p>';
+        }
+    }
+
+    /**
+     * Funcion para editar los datos de un profesor mediante su codigo
+     * @param cod codigo que se pasa por parametro para identificar el profesor que vamos actualizar
+     */
+    public function editProfesor($cod)
+    {
+
+        $sql = "UPDATE profesores set nombre= ?, apellidos= ?, activo= ? where cod_prof = $cod ";
+        $resultado = $this->ejecutaConsulta($sql);
+        $prof = $resultado->execute();
+        if (isset($prof)) {
+            echo '<p><center>Actualizacion completada</center></p>';
+        } else {
+            echo '<p><center>Ha ocurrido un problema</center></p>';
+        }
+
+    }
+
+    /**
+     * Funcion para editar los datos de un alumno
+     * @param cod codigo que se pasa por parametro para identificar el profesor que vamos actualizar
+     */
+    public function editAlumno($cod)
+    {
+        $sql = "UPDATE alumnos set nombre= ?, apellidos= ?, activo= ? where nexped = $cod ";
+        $resultado = $this->ejecutaConsulta($sql);
+        $alum = $resultado->execute();
+        if (isset($alum)) {
+            echo '<p><center>Actualizacion completada</center></p>';
+        } else {
+            echo '<p><center>Ha ocurrido un problema</center></p>';
+        }
+
+    }
+
+
+
+    /**
+     * Funcion para insertar un nuevo profesor
+     * @param cod insertamos el codigo del profesor por parametro
+     * @param nom insertamos el nombre del profesor por parametro
+     * @param ape insertamos el apellido del profesor por parametro
+     * @param act insertamoos por parametro el nº que indica si esta activo o no
+     */
+
+     public function setProfesor($cod, $nom, $ape, $act){
+
+        $sql = "INSERT INTO profesores ('cod_prof', 'nombre', 'apellidos', 'activo') values $cod, $nom, $ape, $act;";
+        $resultado = $this->ejecutaConsulta($sql);
+        $insertar = $resultado->execute();
+        if (isset($insertar)) {
+            echo '<p><center>Inserccion completada</center></p>';
+        } else{
+            echo '<p><center>ERROR!!! No se puede insertar ningun dato</center></p>';
+        }
+
+     }
+
 
 }
--- index.php
+++ index.php
@@ -115,40 +115,90 @@ if (isset($_POST['cod'])) {
 /**
  * Obtendremos todos los profesores con esta lista
  */
-
-if (isset($_POST['cod'])) {
-
-    $profesor = $bd->getProfesorAll();
-    ?>
+$profesor = $bd->getProfesorAll();
+?>
     <div id="Izq">
 		<center><h1> Lista de profesores</h1></center>
-        <?php
+<?php
+
+//Si hay valores dentro de la variable profesor obtendremos una lista de dicho profesor
+if (isset($profesor)) {
+    $lista = "";
+    $lista = "<div center>";
+    $lista .= "<table class='table table-sm'>";
+    $lista .= "<thead class='thead-dark'>";
+    $lista .= "<tr>";
+    $lista .= "<th>cod_prof</th>";
+    $lista .= "<th>Nombre</th>";
+    $lista .= "<th>Apellidos</th>";
+    $lista .= "<th>Activo</th>";
+    $lista .= "</tr>";
+    {
+        foreach ($profesor as $row) {
+
+            $lista .= "<tr>";
+            $lista .= "<td>" . $row['cod_prof'] . "</td>";
+            $lista .= "<td>" . $row['nombre'] . "</td>";
+            $lista .= "<td>" . $row['apellidos'] . "</td>";
+            $lista .= "<td>" . $row['activo'] . "</td>";
+            $lista .= "</tr>";
+        }
+
+        $lista .= "</table>";
+        $lista .= "</div>";
+        echo $lista;
+    }
+    ?>
+	</div>
+<?php
+}
+
+?>
+
+</form>
+<h3><center>Alumno A QUERER MOSTRAR</center></h3>
+<br>
+<form action="./index.php" method="POST">
+<div class="row">
+<div class="col">
+  <input type="number" class="form-control" name="exp" required placeholder="Introduce cod de alumno">
+</div>
+</div>
+<br>
+<button type="submit">Enviar</button>
+</form>
+<?php
+
+//Condicional: Si el input de cod alumno contiene datos realizamos la  llamada a la funcion de getAlumno
+if (isset($_POST['exp'])) {
+
+    $_SESSION['exp'] = $_POST['exp'];
+
+    $alumno = $bd->getAlumno($_SESSION['exp']);
+    ?>
+      <div id="Izq">
+    <?php
 
     //Si hay valores dentro de la variable profesor obtendremos una lista de dicho profesor
-    if (isset($profesor)) {
+    if (isset($alumno)) {
         $lista = "";
-        $lista = "<div center>";
         $lista .= "<table class='table table-sm'>";
         $lista .= "<thead class='thead-dark'>";
         $lista .= "<tr>";
-        $lista .= "<th>cod_prof</th>";
+        $lista .= "<th>nexped</th>";
         $lista .= "<th>Nombre</th>";
         $lista .= "<th>Apellidos</th>";
         $lista .= "<th>Activo</th>";
         $lista .= "</tr>";
         {
-            foreach ($profesor as $row) {
-
-                $lista .= "<tr>";
-                $lista .= "<td>" . $row['cod_prof'] . "</td>";
-                $lista .= "<td>" . $row['nombre'] . "</td>";
-                $lista .= "<td>" . $row['apellidos'] . "</td>";
-                $lista .= "<td>" . $row['activo'] . "</td>";
-                $lista .= "</tr>";
-            }
+            $lista .= "<tr>";
+            $lista .= "<td>" . $alumno['nexped'] . "</td>";
+            $lista .= "<td>" . $alumno['nombre'] . "</td>";
+            $lista .= "<td>" . $alumno['apellidos'] . "</td>";
+            $lista .= "<td>" . $alumno['activo'] . "</td>";
+            $lista .= "</tr>";
 
             $lista .= "</table>";
-            $lista .= "</div>";
             echo $lista;
         }
         ?>
@@ -157,9 +207,292 @@ if (isset($_POST['cod'])) {
 }
 }
 ?>
+<br>
+<br>
+<?php
 
+//Condicional: Si el input de cod profesor contiene datos realizamos la  llamada a la funcion de getProfesorALL
+/**
+ * Obtendremos todos los profesores con esta lista
+ */
+$alumnos = $bd->getAlumnoAll();
+?>
+    <div id="Izq">
+		<center><h1> Lista de alumnos</h1></center>
+<?php
 
+//Si hay valores dentro de la variable profesor obtendremos una lista de dicho profesor
+if (isset($alumnos)) {
+    $lista = "";
+    $lista = "<div center>";
+    $lista .= "<table class='table table-sm'>";
+    $lista .= "<thead class='thead-dark'>";
+    $lista .= "<tr>";
+    $lista .= "<th>nexped</th>";
+    $lista .= "<th>Nombre</th>";
+    $lista .= "<th>Apellidos</th>";
+    $lista .= "<th>Activo</th>";
+    $lista .= "</tr>";
+    {
+        foreach ($alumnos as $row) {
 
+            $lista .= "<tr>";
+            $lista .= "<td>" . $row['nexped'] . "</td>";
+            $lista .= "<td>" . $row['nombre'] . "</td>";
+            $lista .= "<td>" . $row['apellidos'] . "</td>";
+            $lista .= "<td>" . $row['activo'] . "</td>";
+            $lista .= "</tr>";
+        }
+
+        $lista .= "</table>";
+        $lista .= "</div>";
+        echo $lista;
+    }
+    ?>
+	</div>
+<?php
+}
+?>
+
+<!---Obtener los datos de todos los modulos o solo 1--->
+</form>
+<h3><center>Modulo A QUERER MOSTRAR</center></h3>
+<br>
+<form action="./index.php" method="POST">
+<div class="row">
+<div class="col">
+  <input type="number" class="form-control" name="cod_modulo" required placeholder="Introduce cod del modulo">
+</div>
+</div>
+<br>
+<button type="submit">Enviar</button>
+</form>
+<?php
+
+//Condicional: Si el input de cod modulo contiene datos realizamos la  llamada a la funcion de getModulo
+if (isset($_POST['cod_modulo'])) {
+
+    $_SESSION['cod_modulo'] = $_POST['cod_modulo'];
+
+    $modulo = $bd->getModulo($_SESSION['cod_modulo']);
+    ?>
+      <div id="Izq">
+    <?php
+
+    //Si hay valores dentro de la variable modulo obtendremos una lista de dicho modulo
+    if (isset($modulo)) {
+        $lista = "";
+        $lista .= "<table class='table table-sm'>";
+        $lista .= "<thead class='thead-dark'>";
+        $lista .= "<tr>";
+        $lista .= "<th>cod_mod</th>";
+        $lista .= "<th>modulo</th>";
+        $lista .= "<th>descripcion</th>";
+        $lista .= "<th>ciclo</th>";
+        $lista .= "</tr>";
+        {
+            $lista .= "<tr>";
+            $lista .= "<td>" . $modulo['cod_mod'] . "</td>";
+            $lista .= "<td>" . $modulo['modulo'] . "</td>";
+            $lista .= "<td>" . $modulo['descripcion'] . "</td>";
+            $lista .= "<td>" . $modulo['ciclo'] . "</td>";
+            $lista .= "</tr>";
+
+            $lista .= "</table>";
+            echo $lista;
+        }
+        ?>
+	</div>
+<?php
+}
+}
+?>
+<br>
+<br>
+<?php
+
+/**
+ * Obtendremos todos los profesores con esta lista
+ */
+$modulos = $bd->getModuloAll();
+?>
+    <div id="Izq">
+		<center><h1> Lista de modulos</h1></center>
+<?php
+
+//Si hay valores dentro de la variable profesor obtendremos una lista de dicho profesor
+if (isset($modulos)) {
+    $lista = "";
+    $lista = "<div center>";
+    $lista .= "<table class='table table-sm'>";
+    $lista .= "<thead class='thead-dark'>";
+    $lista .= "<tr>";
+    $lista .= "<th>Cod_mod</th>";
+    $lista .= "<th>Modulo</th>";
+    $lista .= "<th>Descripcion</th>";
+    $lista .= "<th>Ciclo</th>";
+    $lista .= "</tr>";
+    {
+        foreach ($modulos as $row) {
+
+            $lista .= "<tr>";
+            $lista .= "<td>" . $row['cod_mod'] . "</td>";
+            $lista .= "<td>" . $row['modulo'] . "</td>";
+            $lista .= "<td>" . $row['descripcion'] . "</td>";
+            $lista .= "<td>" . $row['ciclo'] . "</td>";
+            $lista .= "</tr>";
+        }
+
+        $lista .= "</table>";
+        $lista .= "</div>";
+        echo $lista;
+    }
+    ?>
+	</div>
+<?php
+}
+?>
+<!---Obtener las faltas de un alumno en un modulo--->
+</form>
+<h3><center>Faltas a mostrar</center></h3>
+<br>
+<form action="./index.php" method="POST">
+<div class="row">
+<div class="col">
+  <input type="number" class="form-control" name="codal" required placeholder="Introduce cod del alumno">
+</div>
+<div class="col">
+  <input type="number" class="form-control" name="codmod" required placeholder="Introduce cod del modulo">
+</div>
+</div>
+<br>
+<button type="submit">Enviar</button>
+</form>
+<?php
+
+//Condicional: Si el input de codad y el input de codmod contiene datos realizamos la  llamada a la funcion de getAsistenciasMod
+if (isset($_POST['codal']) && isset($_POST['codmod'])) {
+
+    $_SESSION['codal'] = $_POST['codal'];
+    $_SESSION['codmod'] = $_POST['codmod'];
+
+    $asistencias = $bd->getAsistenciasMod($_SESSION['codal'], $_SESSION['codmod']);
+    ?>
+      <div id="Izq">
+    <?php
+
+    //Si hay valores dentro de la variable modulo obtendremos una lista de dicho modulo
+    if (isset($asistencias)) {
+        $lista = "";
+        $lista .= "<table class='table table-sm'>";
+        $lista .= "<thead class='thead-dark'>";
+        $lista .= "<tr>";
+        $lista .= "<th>cod</th>";
+        $lista .= "<th>fecha</th>";
+        $lista .= "<th>cod_mod</th>";
+        $lista .= "<th>nexped</th>";
+        $lista .= "<th>asistencia</th>";
+        $lista .= "</tr>";
+        {
+            $lista .= "<tr>";
+            $lista .= "<td>" . $asistencias['cod'] . "</td>";
+            $lista .= "<td>" . $asistencias['fecha'] . "</td>";
+            $lista .= "<td>" . $asistencias['cod_mod'] . "</td>";
+            $lista .= "<td>" . $asistencias['nexped'] . "</td>";
+            $lista .= "<td>" . $asistencias['asistencia'] . "</td>";
+            $lista .= "</tr>";
+
+            $lista .= "</table>";
+            echo $lista;
+        }
+        ?>
+	</div>
+<?php
+}
+}
+?>
+<br>
+<br>
+<!---Borrar un profesor--->
+</form>
+<h3><center>Profesor a borrar</center></h3>
+<br>
+<form action="./index.php" method="POST">
+<div class="row">
+<div class="col">
+  <input type="number" class="form-control" name="cod_prof" required placeholder="Introduce cod del profesor">
+</div>
+</div>
+<br>
+<button type="submit">Enviar</button>
+</form>
+
+<?php
+
+if (isset($_POST["cod_prof"])) {
+    $prof = $bd->delProfesor($_POST['cod_prof']);
+
+}
+
+?>
+<br>
+<br>
+<!---Borrar un Alumno--->
+</form>
+<h3><center>Alumno a borrar</center></h3>
+<br>
+<form action="./index.php" method="POST">
+<div class="row">
+<div class="col">
+  <input type="number" class="form-control" name="cod_alumno" required placeholder="Introduce cod del Alumno">
+</div>
+</div>
+<br>
+<button type="submit">Enviar</button>
+</form>
+
+<?php
+
+if (isset($_POST["cod_alumno"])) {
+    $alumno = $bd->delAlumno($_POST['cod_alumno']);
+
+}
+
+?>
+
+<br>
+<br>
+<!---Insertar un profesor--->
+</form>
+<h3><center>Insertar profesores</center></h3>
+<br>
+<form action="./index.php" method="POST">
+<div class="row">
+<div class="col">
+  <input type="number" class="form-control" name="codigoProf" required placeholder="Introduce cod del Profesor">
+</div>
+<div class="col">
+  <input type="text" class="form-control" name="nombreProf" required placeholder="Introduce nombre del Profesor">
+</div>
+<div class="col">
+  <input type="text" class="form-control" name="apellidoProf" required placeholder="Introduce apellidos del Profesor">
+</div>
+<div class="col">
+  <input type="number" class="form-control" name="activoProf" required placeholder="Introduce actvididad del profesor">
+</div>
+</div>
+<br>
+<button type="submit">Enviar</button>
+</form>
+
+<?php
+
+if (isset($_POST['codigoProf']) && isset($_POST['nombreProf']) && isset($_POST['apellidoProf']) && isset($_POST['activoProf'])) {
+    $insertarProf = $bd->setProfesor($_POST['codigoProf'], $_POST['nombreProf'], $_POST['apellidoProf'], $_POST['activoProf']);
+  
+}
+
+?>
 
 
 
